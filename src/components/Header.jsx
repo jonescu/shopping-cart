c76846efc8b4083c3da18692beb7e200aa371f46
Nick Jonescu <nicholasjonescu@gmail.com>
2022-09-27 14:43:59 -0400

    setting up components

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <nav>
+      <ul>
+        <li>Shop</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Header
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+      <h1>Welcome to my Store!</h1>
+      <div className='hero-img'>
+        {/* <img src="" alt="" /> */}
+      </div>
+    </div>
+  )
+}
+
+export default Main
